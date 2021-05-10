c376a595b2ff62aa41614f823bc0fd9a32db1451
seon-hoon-sik <sounhs2000@gmail.com>
2021-05-10 18:19:34 +0900

    프로그래머스 알고리즘(구현)

---
+++ 20210510점프와순간이동/summer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+
+    while (n > 0) {
+        ans += n % 2;
+        n /= 2;
+    }
+
+    return ans;
+}
