d7044eec21d2f3b7d52027da368e31c4299bd74a
seon-hoon-sik <sounhs2000@gmail.com>
2021-05-19 18:34:09 +0900

    백준 알고리즘(자료구조)

---
+++ 20210519최대힙/최대힙.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	priority_queue<int> pq;
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty()) {
+				cout << 0 << '\n';
+			}
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(x);
+		}
+	}
+}
